c8a871ffa1d1524d700b6bc1ec50e68fc6933d9d
Louis Orleans <louis@0rleans.com>
2015-02-02 22:11:41 -0800

    Initial Commit

---
+++ Wallcycle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Wallcycle
+//
+//  Created by Louis Orleans on 2/2/15.
+//  Copyright (c) 2015 Louis Orleans. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
