b4d2f391337d449269ba88399fd63cf35e0b93e5
Louis Orleans <louis@0rleans.com>
2015-02-04 23:45:16 -0800

    Multi Monitor wallpapers work!
    But only if they’re the exact right size.
    
    Bugs
    - monitors[1…n] are all lower quality because the image they receive is
    the full wallpaper plus a little bit :/
    - only works if the image is the exact right size, otherwise it won’t
    attempt to work.
    - split images get saved directly to whatever directory Wallcycle is
    run from (they should get put in a tmp dir)

--- Wallcycle/main.swift
+++ Wallcycle/main.swift
@@ -27,10 +27,7 @@ struct Wallpaper {
 struct Monitor {
     var screen:NSScreen
     var position:Vector2
-}
-
-struct ScreenSetup {
-    var monitors:[Monitor] = []
+    var size:Vector2
 }
 
 public class Wallcycle {
@@ -39,36 +36,66 @@ public class Wallcycle {
     let FOLDERPATH:String = "/Volumes/Files/Pictures/Wallpapers/"
     
     var currentWallpaper:Int = 0
-    var randomize:Bool = true
+    var randomize:Bool = false
     var wallpapers:[Wallpaper] = []
+    var monitors:[Monitor] = []
+    var totalRealestate:Vector2 = Vector2(x:0, y:0)
     
     public func update() {
-        println("timer fired")
+        println("update fired")
         if (randomize) {
             currentWallpaper = Int(arc4random_uniform(UInt32(wallpapers.count)))
-            var imgurl:NSURL = NSURL.fileURLWithPath(wallpapers[currentWallpaper].path)!
-            var error:NSError?
-            
-            workspace.setDesktopImageURL(imgurl, forScreen: leftScreen, options: nil, error: &error)
-            workspace.setDesktopImageURL(imgurl, forScreen: rightScreen, options: nil, error: &error)
-            
+            setWallpaper()
         } else {
             currentWallpaper++
             if (currentWallpaper >= wallpapers.count) {
                 currentWallpaper = 0
             }
-            var imgurl:NSURL = NSURL.fileURLWithPath(wallpapers[currentWallpaper].path)!
-            var error : NSError?
-            
-            workspace.setDesktopImageURL(imgurl, forScreen: leftScreen, options: nil, error: &error)
-            workspace.setDesktopImageURL(imgurl, forScreen: rightScreen, options: nil, error: &error)
-            
+            setWallpaper()
+        }
+    }
+    
+    func setWallpaper () {
+        let wallpaper = wallpapers[currentWallpaper]
+        var imgurl:NSURL = NSURL.fileURLWithPath(wallpaper.path)!
+        var error:NSError?
+        
+        if (wallpaper.multiMonitor) {
+            // TODO make this actually apply mutli monitor wallpapers
+            // TODO: we're gonna want to crop images, and we'll use NSImageRep.setSize(...) for that
+            let wallpaperRatio = wallpaper.size.x / wallpaper.size.y
+            let monitorRatio = totalRealestate.x / totalRealestate.y
+            if (wallpaperRatio == monitorRatio) {
+                var original:NSImage = NSImage(contentsOfFile: wallpaper.path)!
+                var newImages:[NSURL] = []
+                let splitWidth:Int = wallpaper.size.x / monitors.count
+                for i in 0..<monitors.count {
+                    //original.setSize((i * splitWidth) + ((i + 1) * splitWidth), original.size.height)
+                    let img:NSImage = imageResize(original, NSSize(width:(Int(i) * splitWidth) + ((Int(i) + 1) * splitWidth), height:Int(original.size.height)))
+                    let url = NSURL.fileURLWithPath("screen" + String(i) + ".png");
+                    let data:NSData = NSData()
+                    img.TIFFRepresentation?.writeToURL(url!, atomically: false)
+                    //NSFileManager.copyItemAtPath(url!, )
+                    workspace.setDesktopImageURL(url!, forScreen: monitors[i].screen, options: nil, error: &error)
+                    newImages.append(url!)
+                }
+            }
+        } else {
+            for monitor in monitors {
+                //                    var options:NSOptionsKey = NSWorkspaceDesktopImageScalingKey
+                workspace.setDesktopImageURL(imgurl, forScreen: monitor.screen, options: nil, error: &error)
+            }
         }
     }
 
     init() {
-        for screen in NSScreen.screens() {
-            monitors.append(screen: screen as NSScreen, position: Vector2(x: 0, y: 0))
+        if let screens = NSScreen.screens() {
+            for screen in screens {
+                monitors.append(Monitor(screen: screen as NSScreen, position: Vector2(x: 0, y: 0), size: Vector2(x: Int(screen.frame.width), y: Int(screen.frame.height))))
+                // TODO: this won't work if the screens aren't all aligned perfectly in settings (ie: they have any offset)
+                totalRealestate.x += Int(screen.frame.width)
+                totalRealestate.y = (Int(screen.frame.height) > totalRealestate.y) ? Int(screen.frame.height) : totalRealestate.y
+            }
         }
         
         workspace = NSWorkspace.sharedWorkspace()
@@ -91,22 +118,23 @@ public class Wallcycle {
         }
         
 //        NSTimer.scheduledTimerWithTimeInterval(SWITCHTIME, target: self, selector: "update:", userInfo: nil, repeats: true)
-        // TODO this should work, but it doesn't :/ perhaps it doens't work because the main thread ends here
+        // TODO this should work, but it doesn't :/ perhaps it doens't work because the main thread ends gets killed here?
 //        let myTimer = NSTimer(timeInterval: SWITCHTIME, target: self, selector: "update", userInfo: nil, repeats: true)
 //        NSRunLoop.currentRunLoop().addTimer(myTimer, forMode: NSRunLoopCommonModes)
-        
-        let date:NSDate = NSDate()
-        var lastLoopTime:Int = 0 //Int(date.timeIntervalSince1970)
-        while (true) {
-            if ((Int(date.timeIntervalSince1970) - lastLoopTime) * 1000 > Int(SWITCHTIME)) {
-                println("looping this time")
-                update()
-                lastLoopTime = Int(date.timeIntervalSince1970)
-            } else {
-                println(Int(date.timeIntervalSince1970) - lastLoopTime)
-            }
-        }
+        update()
     }
 }
+
+func imageResize (source:NSImage, size:NSSize) -> NSImage {
+    var sourceImage:NSImage = source
+
+    var smallImage:NSImage = NSImage.init(size:size)
+    smallImage.lockFocus()
+    //sourceImage.setSize(size)
+    //currentContext.setImageInterpolation(NSImageInterpolationHigh)
+    sourceImage.drawAtPoint(NSPoint(x:0, y:0), fromRect:CGRectMake(0, 0, size.width, size.height), operation:NSCompositingOperation.CompositeCopy, fraction:1.0)
+    smallImage.unlockFocus()
+    return smallImage
+ }
 
 let wallcycle:Wallcycle = Wallcycle()
