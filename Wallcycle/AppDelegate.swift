4a502b0b38561fb5ca7202dca698e8d388bfe7e7
Louis Orleans <louis@0rleans.com>
2015-03-17 00:41:54 -0700

    More work on the GUI

---
+++ Wallcycle/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  settingsWindow.swift
+//  Wallcycle
+//
+//  Created by Louis Orleans on 3/16/15.
+//  Copyright (c) 2015 Louis Orleans. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet var window: NSWindow
+    
+    func applicationDidFinishLaunching (aNotification: NSNotification?) {
+        
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        
+    }
+}
--- Wallcycle/settings.swift
+++
@@ -1,9 +0,0 @@
-//
-//  settingsWindow.swift
-//  Wallcycle
-//
-//  Created by Louis Orleans on 3/16/15.
-//  Copyright (c) 2015 Louis Orleans. All rights reserved.
-//
-
-import Foundation
